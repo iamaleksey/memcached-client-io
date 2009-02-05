557f8e2c6fc2b3dbb1d84d7656cb40b51253d267
Aleksey Yeschenko <aleksey@yeschenko.com>
2009-02-05 18:13:08 +0700

    Clarify docs

--- Memcached/source/IoMemcached.c
+++ Memcached/source/IoMemcached.c
@@ -6,7 +6,8 @@
 <a href="http://www.danga.com/memcached/">memcached</a> is a high-performance,
 distributed memory object caching system, generic in nature,
 but intended for use in speeding up dynamic web applications
-by alleviating database load.
+by alleviating database load. Memcached is an Io client library for memcached,
+based on C <a href="http://tangent.org/552/libmemcached.html">libmemcached</a>.
 */
 
 #include "IoMemcached.h"
